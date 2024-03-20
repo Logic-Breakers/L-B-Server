7212be29b350aa5b6a4a1c0f8a25eca084688f0d
KSJ-0 <kim.mail1124@gmail.com>
2024-03-20 15:10:14 +0900

    [Fix] token으로 로그인한 memeber 정보 받아오도록 수정

--- airbnb/src/main/java/com/airbnb/airbnb/member/controller/MemberController.java
+++ airbnb/src/main/java/com/airbnb/airbnb/member/controller/MemberController.java
@@ -2,11 +2,13 @@ package com.airbnb.airbnb.member.controller;
 
 import com.airbnb.airbnb.member.dto.MemberPatchDto;
 import com.airbnb.airbnb.member.dto.MemberPostDto;
+import com.airbnb.airbnb.member.entity.Member;
 import com.airbnb.airbnb.member.mapper.MemberMapper;
 import com.airbnb.airbnb.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -25,22 +27,22 @@ public class MemberController {
         return new ResponseEntity<>("계정 생성이 완료되었습니다.", HttpStatus.CREATED);
     }
 
-    @PatchMapping("/{user-id}")
-    public ResponseEntity patchUser(@Valid @RequestBody MemberPatchDto memberPatchDto,
-                                    @PathVariable ("user-id") Long id) {
-        memberService.updateMember(memberPatchDto, id);
+    @PatchMapping()
+    public ResponseEntity patchUser(@AuthenticationPrincipal Member host,
+                                    @Valid @RequestBody MemberPatchDto memberPatchDto) {
+        memberService.updateMember(memberPatchDto, host.getId());
         return new ResponseEntity<>("계정 정보 수정이 완료되었습니다.", HttpStatus.OK);
     }
 
-    @DeleteMapping("/{user-id}")
-    public ResponseEntity deleteUser(@PathVariable ("user-id") Long id) {
-        memberService.removeMember(id);
+    @DeleteMapping()
+    public ResponseEntity deleteUser(@AuthenticationPrincipal Member member) {
+        memberService.removeMember(member.getId());
         return new ResponseEntity<>("계정이 삭제되었습니다.", HttpStatus.OK);
     }
 
-    @GetMapping("/{user-id}")
-    public ResponseEntity getUser (@PathVariable ("user-id") Long id) {
-        return new ResponseEntity<>(memberMapper.toMemberResponseDto(memberService.findVerifiedMember(id)),HttpStatus.OK);
+    @GetMapping()
+    public ResponseEntity getUser (@AuthenticationPrincipal Member member) {
+        return new ResponseEntity<>(memberMapper.toMemberResponseDto(memberService.findVerifiedMember(member)),HttpStatus.OK);
     }
 
 }
